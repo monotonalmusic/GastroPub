9262763ed7c888121a4d8b8bb26298cde8bc7458
Gabriel <gehughes28@gmail.com>
2024-02-22 17:35:04 +0100

    successful generation of menu items, working on generating header and footer in js

---
+++ menu.js
@@ -0,0 +1,37 @@
+async function fetchMenu() {
+    try {
+        const response = await fetch("https://gastropub.webexam-mcdm.dk/api/menus");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        data.forEach(item => {
+            let menuItem = document.createElement("div");
+            menuItem.classList.add("menu-item");
+            menuItem.innerHTML = `
+                <h1 class="item-name">${item.name} (${item.id})</h1>
+                <p class="item-ingredients">${item.ingredients}</p>
+                <p class="item-price">${item.price}</p>
+            `;
+            document.body.appendChild(menuItem);
+
+
+            console.log(item);
+        });
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+    
+}
+
+fetchMenu();
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-let picURL = "https://gastropub.webexam-mcdm.dk/api/images";
-let textURL = "https://gastropub.webexam-mcdm.dk/api/texts";
+const container = document.getElementById("container");
 
-function getResponse (URL) {
-    fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
+function makeHeader () {
+    let header = document.createElement("header");
+    header.id = "header-div";
+    header.innerHTML = 
+    `
+        <div id="header-content">
+            <img id="header-logo" src="gastro-pub.svg">
+            <nav id="header-nav">
+                <ul id="header-list">
+                    <div class="dropdown">
+                        <li><a href="about.html">Om os <i class="arrow down"></i> </a></li>
+                        <div class="dropdown-content">
+                            <a href="about.html">Historie</a>
+                            <a href="about.html">Vores Team</a>
+                        </div>
+                    </div>
+                    <li><a href="menu.html">Menu</a></li>
+                    <li><a href="galleri.html">Galleri</a></li>
+                    <li><a href="kontakt.html">Kontakt</a></li>
+                    <button class="pretty-button">Bestil Bord</button>
+                </ul>
+            </nav>
+        </div>
+    `;
+    container.appendChild(header);
+};
 
-}
+function makeFooter () {
+    let footer = document.createElement("footer");
+    footer.id = "footer-div";
+    footer.innerHTML = 
+    `
+        <div id="footer-content">
+            <div id="info-div">
+                <h2 class="footer-text">Information</h2>
+                <p class="footer-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima quibusdam ipsam ipsa doloremque commodi amet deleniti corporis libero inventore eius.</p>
+            </div>
+            <div id="kontakt-div">
+                <h2 class="footer-text">Kontakt</h2>
+                <p class="footer-text">Telefon: 12345678</p>
+                <p class="footer-text">Adresse: Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div id="social-div">
+                <h2 class="footer-text">Social Media</h2>
+                <div id="social-logo-div">
+                    <img>
+                    <img>
+                    <img>
+                </div>
+            </div>
+        </div>
+    `;
+    container.appendChild(footer);
 
+};
+
+function onLoad () {
+    makeHeader();
+    makeFooter();
+};
+
+onLoad();
